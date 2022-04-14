c9c7113e77722b71b999a032b64533a96a74da5a
nihaal-12 <67818837+nihaal-12@users.noreply.github.com>
2022-04-14 17:16:06 -0400

    Update singular_control_test.py

--- singular_control_test.py
+++ singular_control_test.py
@@ -17,8 +17,9 @@ while sensor_value_check(temp_value) == "False":
     temp_value = recieve_temp_data()
     
 tT = calcuate_tT(t1, t2, t3)
-tss = calculate_tss(temp_value[0], temp_value[1], temp_value[2])  
-tind = calculate_tind(tdss, tss)
+tss1 = calculate_tss(temp_value[0], temp_value[1], temp_value[2])
+#tss2 = calculate_tss(temp_value[3], temp_value[4], temp_value[5])  
+tind = calculate_tind(tdss, tss1)
 tTotal = calculate_tTotal(tind, tT)
  
 #Initialize tTotal timer interupt to achieve desired temperature 
@@ -27,7 +28,7 @@ tTotal = calculate_tTotal(tind, tT)
 
 #Initialize tTotal timer interupt to achieve desired temperature 
 tTotal_timer = Timer(4)
-tTotal_timer.init(mode = Timer.PERIODIC, period = 2 * 60000, callback = data_gathering_callback)
+tTotal_timer.init(mode = Timer.PERIODIC, period = 30 * 60000, callback = data_gathering_callback)
 
 
 # Begin the control 
@@ -38,8 +39,9 @@ while True:
     
 
     temp_value = recieve_temp_data()
-    tss = calculate_tss(temp_value[0], temp_value[1], temp_value[2])  
-    temp_control_check = check_temp(tdss, tss, control_signal)
+    tss1 = calculate_tss(temp_value[0], temp_value[1], temp_value[2])
+    #tss2 = calculate_tss(temp_value[3], temp_value[4], temp_value[5])  
+    temp_control_check = check_temp(tdss, tss1, control_signal)
     
     """
     if temp_control_check == "no":
@@ -60,7 +62,6 @@ while True:
     send_relay_signal_test(len(data), relays)
     
     print("Temperature value: " + str(temp_value))
-    print("Tss value: " + str(tss))
-    sleep(1)
-        
+    print("Tss1 value: " + str(tss1))
+    #print("Tss2 value: " + str(tss2))
         
